c21524d6be851aa2b07d678fc9cf5fe7929f3e15
ThongCaoo <dangthong1412@gmail.com>
2022-05-07 11:28:56 +0700

    hello

--- JavaProject/src/GUI/HomeGUI.java
+++ JavaProject/src/GUI/HomeGUI.java
@@ -24,6 +24,7 @@ public class HomeGUI extends JFrame implements ActionListener{
 	
 	EmployeeGUI emp = new EmployeeGUI();
 	MerchandiseGUI mer = new MerchandiseGUI();
+	ReceiptGUI rec = new ReceiptGUI();
 	
 	public HomeGUI(Employee emp) {
 		this.curEmp = emp;
@@ -179,11 +180,14 @@ public class HomeGUI extends JFrame implements ActionListener{
 		mainHome.add(btnReceipt);
 		
 		add(mainHome);
+		
 	}
 	
 	public void clear() {
 		emp.setVisible(false);
 		mer.setVisible(false);
+		rec.setVisible(false);
+		
 	}
 	
 	public void checkUser(Employee emp) {
@@ -213,6 +217,12 @@ public class HomeGUI extends JFrame implements ActionListener{
 			mer.setVisible(true);
 			add(mer);
 			mer.setBounds(150,120,mer.getWidth(),mer.getHeight());
+		}
+		if(e.getSource() == btnReceipt) {
+			clear();
+			rec.setVisible(true);
+			add(rec);
+			rec.setBounds(150,120,rec.getWidth(),rec.getHeight());
 		}
 	}
 
